75bc0ea95416b7f97f7353805b3239398283a825
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2020-10-25 14:58:01 -0700

    servers

--- Server/src/com/telanoff/californiaholdem/server/Main.java
+++ Server/src/com/telanoff/californiaholdem/server/Main.java
@@ -1,7 +1,38 @@
 package com.telanoff.californiaholdem.server;
 
+import java.net.*;
+import java.io.*;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a Server!!!");
+        while (true) {
+            try {
+                ServerSocket socket = new ServerSocket(12345);
+                Socket client = socket.accept();
+                Random random = new Random();
+
+                System.out.println("Just connected to " + client.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(client.getInputStream());
+
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+                while (client.isConnected()) {
+                    out.write(String.format("%d %d %d %d\n", random.nextInt(450), random.nextInt(255), random.nextInt(450), random.nextInt(255)).getBytes());
+
+                    while (in.available() > 0) {
+                        System.out.print((char)in.readByte());
+                    }
+                }
+
+                socket.close();
+                client.close();
+            } catch (SocketTimeoutException e) {
+                System.out.println("Timed out!");
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
     }
 }
